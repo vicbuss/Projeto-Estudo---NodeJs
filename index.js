ccdbd9c8c5f4ac2c66be38bfee7434f70678cf38
Victor Bussiki <victorbussiki@gmail.com>
2023-02-27 15:31:29 -0300

    Função assync para pegar o conteúdo de um arquivo

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório')); 
+}
+
+//promessa com then
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises.readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch((erro) => trataErro(erro))
+// }
+
+//async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
         console.log(chalk.green(texto));
-    })
+    } catch (erro) {
+        trataErro(erro);    
+    } finally {
+        console.log(chalk.yellow('Operação concluída!'))
+    }
 }
 
 pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
